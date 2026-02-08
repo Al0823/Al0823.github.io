6a6678ef40272a65dbc41219b89c621f0a2028fe
Al0823 <akonyswoodard@gmail.com>
2026-02-08 11:03:23 -0500

    yes

--- javascript/includes/vars.js
+++ javascript/includes/vars.js
@@ -2,13 +2,13 @@ window.vars = {
 
 
 
-  MACVAR: "/Al0823.github.io/javascript/data/",
-  PATHVAR: "/Al0823.github.io/javascript/",
-  INCVAR: "/Al0823.github.io/javascript/includes/",
-  DBVAR: "/Al0823.github.io/javascript/data/",
-  CSSVAR: "/Al0823.github.io/css/style.css",
-  IMGVAR: "/Al0823.github.io/javascript/images/",
-  PAGEVAR: "/Al0823.github.io/javascript/pages/",
+  MACVAR: "/javascript/databases/",
+  PATHVAR: "/javascript/",
+  INCVAR: "/javascript/includes/",
+  DBVAR: "/javascript/data/",
+  CSSVAR: "/css/",
+  IMGVAR: "/javascript/images/",
+  PAGEVAR: "/javascript/pages/",
 
   DEBUGVAR: true,   // [text]DEBUGVAR=1
 
