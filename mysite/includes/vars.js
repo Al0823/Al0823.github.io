d44be8f02860eb335a21265acda9f98638c8ede0
Al0823 <akonyswoodard@gmail.com>
2026-02-11 06:38:57 -0500

    yes

---
+++ mysite/includes/vars.js
@@ -0,0 +1,17 @@
+ 
+	window.vars = {
+
+
+	PATHVAR: "/javascript/",
+  INCVAR: "/javascript/includes/",
+  DBVAR: "/javascript/data/",
+  CSSVAR: "/mysite/css/",
+  IMGVAR: "/javascript/images/",
+  CHARVAR: "/javascript/pages/",
+
+  DEBUGVAR: true,   // [text]DEBUGVAR=1
+
+  WEBSITETITLE: "Javascript Wiki"
+};
+
+	
