27c6fea796613e6f7ae5e24dfa8ca2a01b0b2161
Al0823 <akonyswoodard@gmail.com>
2026-02-15 00:38:52 -0500

    yay

--- mysitespecial/contact/index.pl
+++ mysitespecial/contact/index.pl
@@ -2,98 +2,94 @@
 use strict;
 use warnings;
 use CGI qw(:standard);
-use CGI::Carp qw(fatalsToBrowser);
 
-# Path to includes and images
-my $incVAR = "../includes/";
-my $imgVAR = "../gallery/images/";
+# -------------------------
+# LOAD SITE VARIABLES
+# -------------------------
+do "../includes/vars.inc" or die "Cannot load vars.inc: $!";
 
-print header(-type => 'text/html', -charset => 'UTF-8');
+# -------------------------
+# PAGE TITLE
+# -------------------------
+my $PAGETITLE = "Contact Page";
 
-# Include 90s-style header and navbar
-sub include_file {
-    my ($file) = @_;
-    if (-e $file) {
-        open my $fh, '<', $file or die "Cannot open $file: $!";
-        while (<$fh>) { print; }
-        close $fh;
-    }
-}
-include_file($incVAR . "header.inc");
-include_file($incVAR . "navbar.inc");
+# -------------------------
+# OUTPUT HEADER + NAV
+# -------------------------
+do $incVAR . "header.inc" or die "Cannot load header.inc";
+do $incVAR . "navbar.inc" or die "Cannot load navbar.inc";
 
-print start_html(-title => "Contact Page");
+print header();
+print start_html(-title => $PAGETITLE);
 
-print <<'END_HTML';
-
+# -------------------------
+# PAGE BODY
+# -------------------------
+print qq(
 <table width="100%" cellspacing="10" cellpadding="10">
 <tr>
-  <!-- LEFT INFO COLUMN -->
+
+  <!-- LEFT COLUMN -->
   <td width="20%" valign="top" bgcolor="#808080" style="border:3px solid #000000;">
-    <font size="5">
-    This is my Contact page, the purpose of this page is to allow users to contact me, the creator of the website.
+    <font size="5"><b>Contact Me</b></font><br><br>
+    <font size="3">
+    This is the Contact page. Use this form to reach the creator of the website.
     </font>
   </td>
 
-  <!-- CENTER FORM COLUMN -->
+  <!-- CENTER FORM -->
   <td width="60%" valign="top" bgcolor="#C0C0C0" style="border:3px solid #000000;">
     <center><font size="6"><b>Contact Form</b></font></center><br>
 
-    <form action="thankyou.pl" method="post">
+    <form method="post" action="thankyou.pl">
 
-      <fieldset style="border-color: black;">
-        <legend>Name</legend>
-        First name:<br>
-        <input type="text" name="fname" style="width:400px; height:25px;" required><br><br>
-        Last name:<br>
-        <input type="text" name="lname" style="width:400px; height:25px;" required>
-      </fieldset>
-      <br>
+    <b>Name</b><br><br>
 
-      <fieldset style="border-color: black;">
-        <legend>Contact Information</legend>
-        Email:<br>
-        <input type="text" name="Email" style="width:400px; height:25px;"><br><br>
-        Address:<br>
-        <input type="text" name="address" style="width:400px; height:25px;"><br><br>
-        State:<br>
-        <input type="text" name="state" style="width:400px; height:25px;"><br><br>
-        Phone:<br>
-        <input type="text" name="Phone" style="width:400px; height:25px;">
-      </fieldset>
-      <br>
+    First Name:<br>
+    <input type="text" name="fname" size="40"><br><br>
 
-      <fieldset style="border-color: black;">
-        <legend>Comments</legend>
-        Reason:<br>
-        <select name="Reason" required>
-          <option value="">Select One</option>
-          <option>Report Bug</option>
-          <option>Questions</option>
-          <option>Suggestions</option>
-          <option>Other</option>
-        </select><br><br>
-        Comment:<br>
-        <textarea name="comment" rows="8" cols="50" placeholder="Write your comment here"></textarea>
-      </fieldset>
-      <br>
+    Last Name:<br>
+    <input type="text" name="lname" size="40"><br><br>
 
-      <input type="hidden" name="UserIP" value="<%= $ENV{'REMOTE_ADDR'} %>">
+    <b>Contact Info</b><br><br>
 
-      <input type="submit" value="Submit" style="width:100px; height:30px;">
+    Email:<br>
+    <input type="text" name="Email" size="40"><br><br>
+
+    Phone:<br>
+    <input type="text" name="Phone" size="20"><br><br>
+
+    Address:<br>
+    <input type="text" name="address" size="50"><br><br>
+
+    <b>Reason</b><br>
+    <select name="Reason">
+      <option>Report Bug</option>
+      <option>Questions</option>
+      <option>Suggestions</option>
+      <option>Other</option>
+    </select><br><br>
+
+    <b>Comment</b><br>
+    <textarea name="comment" rows="6" cols="50"></textarea><br><br>
+
+    <input type="submit" value="Submit Form">
+
     </form>
   </td>
 
-  <!-- RIGHT IMAGE COLUMN -->
+  <!-- RIGHT IMAGE -->
   <td width="20%" valign="top" bgcolor="#808080" style="border:3px solid #000000; text-align:center;">
-    <img src="../images/contact.png" width="180" alt="Contact Image"><br>
-    <font size="2">Contact Image</font>
+    <img src="../gallery/images/contact.png" width="200" alt="Contact"><br>
+    <font size="2">Contact Page</font>
   </td>
+
 </tr>
 </table>
+);
 
-END_HTML
-
-# Include 90s-style footer
-include_file($incVAR . "footer.inc");
-include_file($incVAR . "debug.inc") if $ENV{'DEBUG'};
+# -------------------------
+# FOOTER + DEBUG
+# -------------------------
+do $incVAR . "footer.inc" or die "Cannot load footer.inc";
+do $incVAR . "debug.inc" if $debugVAR == 1;
