8286764396b3438e723a9dae8228d7203df21769
Al0823 <akonyswoodard@gmail.com>
2026-02-20 18:48:09 -0500

    yes

--- shaysanimalshelter/js/includes.js
+++ shaysanimalshelter/js/includes.js
@@ -1,15 +1,21 @@
+// js/includes.js
 (function() {
   const vars = window.vars;
   if (!vars) return;
 
   function loadInclude(id, file) {
+    console.log("Loading include:", file); // debug
     fetch(vars.INCVAR + file)
-      .then(res => res.text())
+      .then(res => {
+        if (!res.ok) throw new Error(res.status + " " + res.statusText);
+        return res.text();
+      })
       .then(html => {
         const el = document.getElementById(id);
         if (el) el.innerHTML = html;
+        else console.error("No element with id:", id);
       })
-      .catch(console.error);
+      .catch(err => console.error("Include load error:", file, err));
   }
 
   document.addEventListener("DOMContentLoaded", () => {
