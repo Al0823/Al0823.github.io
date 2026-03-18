a1a6a0adfb22be4fc4af77bf440f476dde4df9a7
Al0823 <akonyswoodard@gmail.com>
2026-03-18 10:40:44 -0400

    ok

--- myproject/js/vars.js
+++ myproject/js/vars.js
@@ -1,18 +1,14 @@
-// ../includes/vars.js
 window.vars = (function() {
-  var PATHVAR = "/myproject/";
-
-  return {
-    PATHVAR: PATHVAR,                  // base path
-    INCVAR: PATHVAR + "includes/",     // includes folder
-    DBVAR: PATHVAR + "data/",          // data folder
-    CSSVAR: "/css/",                    // CSS folder (optional, outside project folder)
-    IMGVAR: PATHVAR + "images/",       // images folder
-    PAGEVAR: PATHVAR + "pages/",       // pages folder
-    JSVAR: PATHVAR + "js/",            // JS folder
-
-    DEBUGVAR: false,                   // debug mode
-
-    WEBSITETITLE: "A.t.A. Tech Wiki"  // site-wide title
-  };
+var PATHVAR = "/myproject/";
+return {
+PATHVAR: PATHVAR,
+INCVAR: PATHVAR + "includes/",
+DBVAR: PATHVAR + "data/",
+CSSVAR: "/css/",
+IMGVAR: PATHVAR + "images/",
+PAGEVAR: PATHVAR + "pages/",
+JSVAR: PATHVAR + "js/",
+AUDIOVAR: "/audio/",
+DEBUGVAR: false,
+WEBSITETITLE: "A.t.A. Tech Wiki"};
 })();
