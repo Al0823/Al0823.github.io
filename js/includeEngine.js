b2afc6e4345e5754944e208b51cb49872e10b97b
Al0823 <akonyswoodard@gmail.com>
2026-03-19 08:28:15 -0400

    please please please

--- js/includeEngine.js
+++ js/includeEngine.js
@@ -1,72 +1,137 @@
 (function () {
-var w = window;
-var d = document;
+  var w = window;
+  var d = document;
 
-var config = { delayMin: 20, delayMax: 100, shuffle: true };
+  var config = {
+    delayMin: 20,
+    delayMax: 100,
+    shuffle: true
+  };
 
-function random(min, max) { return Math.random() * (max - min) + min; }
-function delay(fn, time) { setTimeout(fn, time); }
-function shuffleArray(arr) {
-var i = arr.length, j, temp;
-while (i--) { j = Math.floor(Math.random() * arr.length); temp = arr[i]; arr[i] = arr[j]; arr[j] = temp; }
-return arr;
-}
+  function random(min, max) {
+    return Math.random() * (max - min) + min;
+  }
 
-function include(targetId, url, callback) {
-try {
-var iframe = d.createElement("iframe");
-iframe.style.display = "none";
-iframe.src = url;
+  function delay(fn, time) {
+    setTimeout(fn, time);
+  }
 
-iframe.onload = function () {
-var target = d.getElementById(targetId);
-if (!target) { if (callback) callback(); return; }
+  function shuffleArray(arr) {
+    var i = arr.length, j, temp;
+    while (i--) {
+      j = Math.floor(Math.random() * arr.length);
+      temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+    return arr;
+  }
 
-var doc = iframe.contentDocument || iframe.contentWindow.document;
-target.innerHTML = doc.body.innerHTML;
+  function include(targetId, url, callback) {
+    fetch(url)
+      .then(function (res) {
+        if (!res.ok) throw new Error("Failed to load: " + url);
+        return res.text();
+      })
+      .then(function (html) {
+        var target = d.getElementById(targetId);
+        if (!target) {
+          if (callback) callback();
+          return;
+        }
 
-var scripts = doc.getElementsByTagName("script");
-for (var i = 0; i < scripts.length; i++) {
-var s = d.createElement("script");
-try { s.appendChild(d.createTextNode(scripts[i].innerHTML)); } catch (e) { s.text = scripts[i].innerHTML; }
-(d.body || d.documentElement).appendChild(s);
-}
+        // Create a container to parse HTML safely
+        var temp = d.createElement("div");
+        temp.innerHTML = html;
 
-if (callback) callback();
-d.body.removeChild(iframe);
-};
+        // Extract and move non-script content
+        var nodes = temp.childNodes;
+        var scripts = [];
 
-d.body.appendChild(iframe);
-} catch (e) { if (callback) callback(); }
-}
+        // Collect scripts separately
+        temp.querySelectorAll("script").forEach(function (s) {
+          scripts.push(s);
+        });
 
-function loadIncludes(list, done) {
-if (!list || !list.length) { if (done) done(); return; }
-if (config.shuffle) list = shuffleArray(list);
+        // Remove scripts from DOM before injection
+        scripts.forEach(function (s) {
+          s.parentNode.removeChild(s);
+        });
 
-var i = 0;
-function next() {
-if (i >= list.length) {
-if (done) done();
+        // Inject HTML (without scripts)
+        target.innerHTML = "";
+        while (temp.firstChild) {
+          target.appendChild(temp.firstChild);
+        }
 
+        // Execute external scripts only (CSP-safe)
+        scripts.forEach(function (oldScript) {
+          var newScript = d.createElement("script");
 
-try {
-if (typeof w.PAGETITLE !== "undefined" && typeof w.vars !== "undefined") {
-document.title = vars.WEBSITETITLE + " - " + PAGETITLE;
-}
-} catch (e) {}
-return;
-}
+          // Copy attributes
+          for (var i = 0; i < oldScript.attributes.length; i++) {
+            var attr = oldScript.attributes[i];
+            newScript.setAttribute(attr.name, attr.value);
+          }
 
-var item = list[i++];
-delay(function () { include(item.id, w.vars.INCVAR + item.file, next); }, random(config.delayMin, config.delayMax));
-}
+          // Only external scripts are loaded
+          if (oldScript.src) {
+            newScript.src = oldScript.src;
+          } else {
+            // Inline scripts are skipped (CSP-safe approach)
+            console.warn("Inline script skipped for CSP safety:", url);
+          }
 
-next();
-}
+          (d.body || d.documentElement).appendChild(newScript);
+        });
 
-w.IncludeEngine = {
-load: function (list, callback) { loadIncludes(list, callback); },
-single: function (id, file, callback) { include(id, w.vars.INCVAR + file, callback); }
-};
+        if (callback) callback();
+      })
+      .catch(function (err) {
+        console.error(err);
+        if (callback) callback();
+      });
+  }
+
+  function loadIncludes(list, done) {
+    if (!list || !list.length) {
+      if (done) done();
+      return;
+    }
+
+    if (config.shuffle) list = shuffleArray(list);
+
+    var i = 0;
+
+    function next() {
+      if (i >= list.length) {
+        if (done) done();
+
+        try {
+          if (typeof w.PAGETITLE !== "undefined" && w.vars) {
+            document.title = w.vars.WEBSITETITLE + " - " + w.PAGETITLE;
+          }
+        } catch (e) {}
+
+        return;
+      }
+
+      var item = list[i++];
+
+      delay(function () {
+        include(item.id, w.vars.INCVAR + item.file, next);
+      }, random(config.delayMin, config.delayMax));
+    }
+
+    next();
+  }
+
+  w.IncludeEngine = {
+    load: function (list, callback) {
+      loadIncludes(list, callback);
+    },
+    single: function (id, file, callback) {
+      include(id, w.vars.INCVAR + file, callback);
+    }
+  };
 })();
