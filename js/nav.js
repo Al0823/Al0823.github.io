e68d9ec4351669b366c198fdb92ceecff87f0992
Al0823 <akonyswoodard@gmail.com>
2026-03-19 08:23:56 -0400

    fuck

--- js/nav.js
+++ js/nav.js
@@ -2,13 +2,11 @@
 
   function waitForReady(callback) {
 
-    // wait for vars.js
     if (!window.vars || !window.vars.DBVAR || !window.vars.PATHVAR) {
       setTimeout(function() { waitForReady(callback); }, 50);
       return;
     }
 
-    // wait for nav container
     var nav = document.getElementById("navList");
     if (!nav) {
       setTimeout(function() { waitForReady(callback); }, 50);
@@ -26,7 +24,6 @@
       })
       .then(function(data) {
 
-        // sort by sortOrder
         data.sort(function(a, b) {
           return a.sortOrder - b.sortOrder;
         });
@@ -46,7 +43,6 @@
     for (var i = 0; i < items.length; i++) {
       var item = items[i];
 
-      // only active items
       if (item.status !== 1) continue;
 
       html += '<li><a href="' +
@@ -59,7 +55,7 @@
     return html;
   }
 
-  // start engine
+
   waitForReady(loadNav);
 
 })();
