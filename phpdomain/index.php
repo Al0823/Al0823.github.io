b9559af534ad71ca9306b327e36768b5aa8bf047
Al0823 <akonyswoodard@gmail.com>
2026-04-12 21:27:19 +0000

    k

--- phpdomain/index.php
+++ phpdomain/index.php
@@ -11,8 +11,8 @@ $ALTDOMAINVAR = "https://al0823.github.io";
 $ASSIGNMENTSVAR   = "/lessons/assignments/";
 $DECODERVAR   = "/datawindecoder/v3/";
 $AIVAR   = "/ai/20.0/";
-$RUNNERVAR   = "/coderunner/";
-$GAMEVAR   = "/game/game/";
+$RUNNERVAR   = "/coderunner/index.html";
+$GAMEVAR   = "/game/game/index.html";
 $DEBUGVAR     = 0;
 ?>
 
@@ -25,129 +25,138 @@ $DEBUGVAR     = 0;
 <body bgcolor="#afafaf" text="#000000" link="#4b5c01" vlink="#4b5c01" alink="#000000">
 <center>
 
-<table width="760" border="0" cellpadding="0" cellspacing="0">
-<tr>
-<td bgcolor="#4b4b4b">
+<!-- TOP MARQUEE -->
+<table width="760"><tr><td bgcolor="#4b4b4b">
 <marquee scrollamount="4">
-<span style="color:#ffffff;font-family:Arial;">*** WELCOME TO A.t.A. PROJECTS ***</span>
+<font color="#ffffff" size="3" face="Arial, sans-serif">
+*** WELCOME TO A.t.A. PROJECTS *** *** BEST VIEWED IN NETSCAPE NAVIGATOR 4.0 OR INTERNET EXPLORER 5 *** *** 800x600 RESOLUTION RECOMMENDED ***
+</font>
 </marquee>
-</td>
-</tr>
-</table>
+</td></tr></table>
 
-<table width="760" border="0" cellpadding="0" cellspacing="0" bgcolor="#4b4b4b">
-<tr>
-<td width="80" align="center">
-<span style="color:#ffffff;font-size:xx-large;">★</span><br>
-<span style="color:#c8c8c8;font-size:small;">Est. 2024</span>
-</td>
-<td align="center">
-<span style="color:#ffffff;font-family:Comic Sans MS;font-size:300%;"><b>A.t.A. Projects</b></span><br>
-<span style="color:#c8c8c8;font-family:Courier New;"><i>~ Your #1 Source For My Projects ~</i></span>
-</td>
-<td width="80" align="center">
-<span style="color:#ffffff;font-size:xx-large;">★</span><br>
-<span style="color:#c8c8c8;font-size:small;">Est. 2024</span>
-</td>
-</tr>
-</table>
-
-<br>
-
-<table width="760" border="0" cellpadding="6" cellspacing="4">
-<tr valign="top">
-
-<td width="370" bgcolor="#c8c8c8">
-
-<b>► 1st Year Projects</b><br><br>
-
-• <?php echo "<a href='".$ALTDOMAINVAR.$PROPOSALVAR."index.html' target='_blank'><b>Proposal Website (Mysite Project Idea)</b></a>"; ?><br><br>
-
-• <?php echo "<a href='".$MYSITEVAR."index.html' target='_blank'><b>Murder Drones Fan Wiki (Mysite Project)</b></a>"; ?><br><br>
-
-• <?php echo "<a href='".$AMPVAR."index.html' target='_blank'><b>AMP Project</b></a>"; ?><br><br>
-
-• <a href="/tbgp7/index.html" target="_blank"><b>One Wrong Choice (Text Based Game)</b></a><br><br>
-
-<b>► 2nd Year Projects</b><br><br>
-
-• <?php echo "<a href='".$ALTDOMAINVAR.$JSWIKIVAR."index.html' target='_blank'><b>Javascript Wiki</b></a>"; ?><br><br>
-
-• <?php echo "<a href='".$WIKIVAR."index.html' target='_blank'><b>A.T.A. Tech Wiki</b></a>"; ?><br><br>
-
-• <?php echo "<a href='".$WIKIDEVVAR."index.html' target='_blank'><b>A.T.A. Tech Wiki Dev Page</b></a>"; ?><br><br>
-
-• <?php echo "<a href='".$ALTDOMAINVAR.$ASSIGNMENTSVAR."week1.html' target='_blank'><b>CSS Driven Form</b></a>"; ?><br><br>
-
-• <?php echo "<a href='".$ASSIGNMENTSVAR."week2.html' target='_blank'><b>Simple WebDNA Admin</b></a>"; ?>
-<span style="color:#ff0000;font-size:xx-small;">[WebDNA replaced with PHP]</span><br><br>
-
-• <?php echo "<a href='".$ALTDOMAINVAR.$ASSIGNMENTSVAR."week3/index.html' target='_blank'><b>Hangman Splash Screen</b></a>"; ?><br><br>
-
-• <?php echo "<a href='".$ASSIGNMENTSVAR."hangman/index.html' target='_blank'><b>Hangman Game</b></a>"; ?><br><br>
-
-</td>
-
-<td width="230" bgcolor="#c8c8c8">
-
-<b>► Other Projects</b><br><br>
-
-• <?php echo "<a href='".$ALTDOMAINVAR.$DECODERVAR."index.html' target='_blank'><b>Data.win Decoder (W.I.P.)</b></a>"; ?><br><br>
-
-• <?php echo "<a href='".$ALTDOMAINVAR.$AIVAR."index.html' target='_blank'><b>FreeGPT (W.I.P.)</b></a>"; ?><br><br>
-
-• <?php echo "<a href='".$ALTDOMAINVAR.$RUNNERVAR."index.html' target='_blank'><b>Code Runner</b></a>"; ?><br><br>
-
-• <?php echo "<a href='".$ALTDOMAINVAR.$GAMEVAR."index.html' target='_blank'><b>Unnamed Text-Based Game</b></a>"; ?><br><br>
-
-<b>► Other Domains/Servers</b><br><br>
-
-• <?php echo "<a href='".$ALTDOMAINVAR."/index.html' target='_blank'><b>My Static Domain</b></a>"; ?><br>
-<span style="color:#4b4b4b;font-size:xx-small;">Check out my Other Domain</span><br><br>
-
-</td>
-
-</tr>
-</table>
-
-<br>
-
-<?php
-$counterFile = "counter.txt";
-
-if (!file_exists($counterFile)) {
-    file_put_contents($counterFile, "000000");
-}
-
-$count = (int)file_get_contents($counterFile);
-$count++;
-file_put_contents($counterFile, str_pad($count, 6, "0", STR_PAD_LEFT));
-
-echo "<center>".str_pad($count, 6, "0", STR_PAD_LEFT)."</center>";
-?>
-
-<br>
-
+<!-- HEADER -->
 <table width="760" bgcolor="#4b4b4b">
 <tr>
+<td width="80" align="center">
+<font size="6" color="#ffffff">&#9733;</font><br>
+<font size="2" color="#c8c8c8">Est. 2024</font>
+</td>
 <td align="center">
-<span style="color:#c8c8c8;font-size:12px;">
-© 2024 - <script>document.write(new Date().getFullYear());</script>
-</span>
+<font face="Comic Sans MS" size="7" color="#ffffff"><b>A.t.A. Projects</b></font><br>
+<font face="Courier New" size="3" color="#c8c8c8"><i>~ Your #1 Source For My Projects ~</i></font><br><br>
+<blink><font size="2" color="#ffffff">&#9733; WELCOME TO MY WEBSITE &#9733;</font></blink>
+</td>
+<td width="80" align="center">
+<font size="6" color="#ffffff">&#9733;</font><br>
+<font size="2" color="#c8c8c8">Est. 2024</font>
 </td>
 </tr>
 </table>
 
-<?php
-if ($DEBUGVAR == 1) {
-    echo "<hr><b>DEBUG:</b><br>";
-    foreach (get_defined_vars() as $k => $v) {
-        if (!is_array($v) && !is_object($v)) {
-            echo $k." = ".$v."<br>";
-        }
-    }
-}
-?>
+<!-- WARNING -->
+<table width="760" border="2" bgcolor="#fff3cd">
+<tr><td align="center">
+<font size="2" color="#c70039"><b>&#9888; THIS SITE IS UNDER CONSTRUCTION &#9888;</b> Some links may be broken.</font>
+</td></tr>
+</table>
+
+<br>
+
+<table width="760" cellpadding="6" cellspacing="4">
+<tr valign="top">
+
+<!-- LEFT -->
+<td width="370" bgcolor="#c8c8c8">
+
+<font size="3"><b>&#9658; 1st Year Projects</b></font><br><br>
+
+&#149; <?php echo "<a href='".$PROPOSALVAR."index.html' target='_blank'><b>Proposal Website</b></a>"; ?><br>
+<font size="1">My very first web project!</font><br><br>
+
+&#149; <?php echo "<a href='".$MYSITEVAR."index.html' target='_blank'><b>Murder Drones Fan Wiki</b></a>"; ?><br>
+<font size="1">Fan wiki for Murder Drones!</font><br><br>
+
+&#149; <?php echo "<a href='".$AMPVAR."index.html'><b>AMP Project</b></a>"; ?><br><br>
+
+&#149; <?php echo "<a href='".$TBGVAR."index.html'><b>One Wrong Choice</b></a>"; ?><br><br>
+
+<font size="3"><b>&#9658; 2nd Year Projects</b></font><br><br>
+
+&#149; <?php echo "<a href='".$JSWIKIVAR."index.html'><b>Javascript Wiki</b></a>"; ?><br><br>
+
+&#149; <?php echo "<a href='".$WIKIVAR."index.html'><b>A.T.A. Tech Wiki</b></a>"; ?><br><br>
+
+&#149; <?php echo "<a href='".$WIKIDEVVAR."index.html'><b>A.T.A. Tech Wiki Dev Page</b></a>"; ?><br><br>
+
+&#149; <?php echo "<a href='".$ASSIGNMENTSVAR."week1.html'><b>CSS Driven Form</b></a>"; ?><br><br>
+
+&#149; <?php echo "<a href='".$ASSIGNMENTSVAR."week2.html'><b>Simple WebDNA Admin</b></a>"; ?><br>
+
+<font size="1" color="#ff0000">[WebDNA replaced with PHP]</font><br><br>
+
+&#149; <?php echo "<a href='".$ASSIGNMENTSVAR."week3/index.html'><b>Hangman Splash Screen</b></a>"; ?><br><br>
+
+&#149; <?php echo "<a href='".$ASSIGNMENTSVAR."hangman/index.html'><b>Hangman Game</b></a>"; ?><br><br>
+
+</td>
+
+<!-- RIGHT -->
+<td width="230" bgcolor="#c8c8c8">
+
+<font size="3"><b>&#9658; Client Projects</b></font><br><br>
+
+&#149; <?php echo "<a href='".$SHSVAR."index.html'><b>Shay's Animal Shelter</b></a>"; ?><br>
+<font size="1">Help animals find homes!</font><br><br>
+
+<font size="3"><b>&#9658; Other Projects</b></font><br><br>
+
+&#149; <?php echo "<a href='".$DECODERVAR."index.html'><b>Data.win Decoder(W.I.P.)</b></a>"; ?><br><br>
+
+&#149; <?php echo "<a href='".$AIVAR."index.html'><b>FreeGPT(W.I.P.)</b></a>"; ?><br><br>
+
+&#149; <?php echo "<a href='".$RUNNERVAR."'><b>Static Code Runner(W.I.P.)</b></a>"; ?><br><br>
+
+&#149; <?php echo "<a href='".$GAMEVAR."'><b>Unnamed Text-Based Game(W.I.P.)</b></a>"; ?><br><br>
+
+<font size="3"><b>&#9658; Other Domains/Servers</b></font><br><br>
+
+&#149; <?php echo "<a href='https://aloysiusprojects.kesug.com/index.html'><b>My Other Domain</b></a>"; ?><br>
+<font size="1">Check out my PHP Domain</font><br><br>
+
+<!-- EMAIL -->
+<table width="100%" border="2" bgcolor="#e6e6e6">
+<tr><td align="center">
+<font size="2"><b>&#9993; Email Me!</b><br>
+<a href="mailto:akonyswoodard@gmail.com">akonyswoodard@gmail.com</a>
+</font>
+</td></tr>
+</table>
+
+<br>
+
+<!-- STATS -->
+<table width="100%" border="2">
+<tr><td>
+<font size="1"><b>&#9733; Site Stats &#9733;</b><br>
+Websites: <b>12</b>
+</font>
+</td></tr>
+</table>
+
+</td>
+</tr>
+</table>
+
+<br>
+
+<!-- FOOTER MARQUEE -->
+<table width="760"><tr><td bgcolor="#4b4b4b">
+<marquee direction="right">
+<font color="#ffffff">
+Thanks for visiting! • Come back soon!
+</font>
+</marquee>
+</td></tr></table>
 
 </center>
 </body>
