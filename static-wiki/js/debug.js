42cb09a6e896b01075157a753c897135e59f5fbd
Al0823 <akonyswoodard@gmail.com>
2026-05-03 15:52:56 -0400

    ok

--- static-wiki/js/debug.js
+++ static-wiki/js/debug.js
@@ -39,8 +39,8 @@ function runDebug() {
   }
 
   jsDebugDiv.innerHTML = debugVarNames
-    .map((name, i) => `${i}&nbsp;&nbsp;<b>${name}</b> = ${getValue(name)}`)
-    .join("<br><hr>");
+    .map((name, i) => `${i}<b>${name}</b> = ${getValue(name)}`)
+    .join("<br><br><hr><br>");
 
   function getAllCSSVars() {
     const found = [];
@@ -63,8 +63,8 @@ function runDebug() {
   cssDebugDiv.innerHTML = allVars.length
     ? allVars.map((name, i) => {
         const value = getComputedStyle(document.documentElement).getPropertyValue(name).trim();
-        return `${i}&nbsp;&nbsp;<b>${name}</b> = ${value}`;
-      }).join("<br><hr>")
+        return `${i}<b>${name}</b> = ${value}`;
+      }).join("<br><br><hr><br>")
     : "(no CSS custom properties found on :root)";
 }
 
